a436ae739d037f8e09dd5443ae58569d5a3903a5
Paul Leugemors <pleugemors@yahoo.com>
2019-12-09 13:13:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.size == 2 
     return "#{array[0]} and #{array[1]}"
   elsif array.size > 2
-    return array[0..array.size-2].join(", ")+ " and " + array[-1] 
+    return array[0..array.size-2].join(", ")+ ", and " + array[-1] 
   else
     return array
   end
