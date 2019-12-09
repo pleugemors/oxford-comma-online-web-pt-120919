fb761ab8115ca80b198d5f7e94741e100ced803c
Paul Leugemors <pleugemors@yahoo.com>
2019-12-09 13:12:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
   if array.size == 2 
-    return "#{array[o]} and #{array[1]}"
+    return "#{array[0]} and #{array[1]}"
   elsif array.size > 2
     return array[0..array.size-2].join(", ")+ "and " + array[-1] 
   else
