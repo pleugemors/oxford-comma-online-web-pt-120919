992b7bccb12953578e9e88ca84726f48a1769201
Paul Leugemors <pleugemors@yahoo.com>
2019-12-09 13:14:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.size == 2 
+    return "#{array[0]} and #{array[1]}"
+  elsif array.size > 2
+    return array[0..array.size-2].join(", ")+ ", and " + array[-1] 
+  else
+    return array[0]
+  end
 end
